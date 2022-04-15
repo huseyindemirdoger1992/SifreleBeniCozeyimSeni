848e268388e4cb74cc0771050e66eacdc8a93115
husey <husey@MYC>
2022-04-15 06:52:30 +0300

    Add project files.

---
+++ SifreleBeniCozeyimSeni/Models/Hadi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SifreleBeniCozeyimSeni.Models
+{
+    public class Hadi
+    {
+        public string Sifrele(string Deger)
+        {
+            string Sonuc = null;
+            char[] _Deger = Deger.ToCharArray();
+            foreach (char tane in _Deger)
+            {
+                Sonuc += Convert.ToChar(tane - 8).ToString();
+            }
+            return Sonuc;
+        }
+        public string Cozumle(string SifrelenmisDeger)
+        {
+            string deger = null;
+            char[] _SifrelenmisDeger = SifrelenmisDeger.ToCharArray();
+            foreach (char tane in _SifrelenmisDeger)
+            {
+                deger += Convert.ToChar(tane + 8).ToString();
+            }
+            return deger;
+        }
+    }
+}
